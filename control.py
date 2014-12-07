ad8e4c78972bd48b0b46a57fc90cebb6542c4ca9
utommo <thomasthuesen@hotmail.com>
2014-12-07 22:15:59 +0100

    Current version
    Old files from 2013

---
+++ control.py
@@ -0,0 +1,105 @@
+#!/usr/bin/pythonRoot
+# bring in the libraries
+import RPi.GPIO as G     
+from flup.server.fcgi import WSGIServer 
+import sys, urlparse
+import os
+ 
+#Setting up the GPIO pins
+#Use the 'BCM' numbering scheme for the pins
+G.setmode(G.BCM)         
+
+#Motor enable
+G.setup(30, G.OUT)
+G.setup(17, G.OUT)
+G.setup(21, G.OUT)
+G.setup(22, G.OUT)
+
+G.output(30, True)
+G.output(17, True)
+G.output(21, True)
+G.output(22, True)
+
+#Forward and reverse
+#--Right
+G.setup(18, G.OUT)
+G.setup(23, G.OUT)
+#--Left
+G.setup(24, G.OUT)
+G.setup(25, G.OUT)
+
+#Servo definitions 
+os_posi1 = "echo 4=0.245 > /dev/pi-blaster"
+os_posi2 = "echo 4=0.22 > /dev/pi-blaster"
+os_posi3 = "echo 4=0.20 > /dev/pi-blaster"
+os_posi4 = "echo 4=0.18 > /dev/pi-blaster"
+os_posi5 = "echo 4=0.16 > /dev/pi-blaster"
+os_posi6 = "echo 4=0.14 > /dev/pi-blaster"
+os_posi7 = "echo 4=0.12 > /dev/pi-blaster"
+os_posi8 = "echo 4=0.10 > /dev/pi-blaster"
+
+#Run cam
+os.system('mjpg_streamer -i "/usr/lib/input_uvc.so -d /dev/video0 -r 640x480 -f 30 -n" -o "/usr/lib/output_http.so -p 8090 -w /var/www/" -b')
+
+# all of our code now lives within the app() function which is called for each http request we receive
+def app(environ, start_response):
+  # start our http response 
+  start_response("200 OK", [("Content-Type", "text/html")])
+  # look for inputs on the URL
+  i = urlparse.parse_qs(environ["QUERY_STRING"])
+  yield ('&nbsp;') # flup expects a string to be returned from this function
+  # if there's a url variable named 'q'
+  if "q" in i:
+    if i["q"][0] == "w": 
+        G.output(18, False)
+    	G.output(23, True)
+        G.output(24, False)
+        G.output(25, True)
+    elif i["q"][0] == "d":
+        G.output(18, False)
+        G.output(23, True)
+        G.output(24, True)
+        G.output(25, False)
+    elif i["q"][0] == "a":
+        G.output(24, False)
+        G.output(25, True)
+        G.output(18, True)
+        G.output(23, False)
+    elif i["q"][0] == "s":
+        G.output(18, True)
+        G.output(23, False)
+        G.output(24, True)
+        G.output(25, False)
+    elif i["q"][0] == "release":
+        G.output(23, False)
+    	G.output(18, False)
+        G.output(25, False)
+        G.output(24, False)
+    elif i["q"][0] == "halt":
+        G.output(23, False)
+        G.output(18, False)
+        G.output(25, False)
+        G.output(24, False)
+
+
+    elif i["q"][0] == "posi1":
+	   os.system(os_posi1)
+    elif i["q"][0] == "posi2":
+        os.system(os_posi2)
+    elif i["q"][0] == "posi3":
+        os.system(os_posi3)
+    elif i["q"][0] == "posi4":
+        os.system(os_posi4)
+    elif i["q"][0] == "posi5":
+        os.system(os_posi5)
+    elif i["q"][0] == "posi6":
+        os.system(os_posi6)
+    elif i["q"][0] == "posi7":
+        os.system(os_posi7)
+    elif i["q"][0] == "posi8":
+        os.system(os_posi8)
+    elif i["q"][0] == "reboot":
+        os.system("echo sudo shutdown -r now")
+ 
+#by default, Flup works out how to bind to the web server for us, so just call it with our app() function and let it get on with it
+WSGIServer(app).run()
---
+++ keyboard.js
@@ -0,0 +1,267 @@
+document.onkeydown = KeyCheckKeyDown;
+document.onkeyup = KeyCheckKeyUp;
+
+window.onload = function()
+                {
+                   document.getElementById("imagesrc").src="http://" + window.location.hostname + ":8090/?action=stream";
+                };
+
+var checkifactive = false;
+var checkdebug = false;
+var checkextra = false;
+var keyNUM = 00;
+
+var enabledE = false;
+var enabledQ = false;
+
+function Reboot(){
+	new Ajax.Request('control.py?q=reboot', {method: 'GET'});
+	debug_text.value += "Rebooting Device..." + "\n";
+	
+	document.getElementById('actiontext').innerHTML = 'Current action:';
+	var textarea = document.getElementById('debug_text');
+	textarea.scrollTop = textarea.scrollHeight- textarea.clientHeight;
+}
+
+function ResetTextarea(){
+
+	debug_text.value = '';
+
+}
+function ResetOutputs(){
+	new Ajax.Request('control.py?q=halt', {method: 'GET'});
+	debug_text.value += "FORCE STOP" + "\n";
+	
+	document.getElementById('actiontext').innerHTML = 'Current action:';
+	var textarea = document.getElementById('debug_text');
+	textarea.scrollTop = textarea.scrollHeight- textarea.clientHeight;
+}
+function CamPosi0(){
+	new Ajax.Request('control.py?q=posi0', {method: 'GET'});
+	debug_text.value += "Cam Posi 0" + "\n";
+	
+	document.getElementById('actiontext').innerHTML = 'Current action:';
+	var textarea = document.getElementById('debug_text');
+	textarea.scrollTop = textarea.scrollHeight- textarea.clientHeight;
+}
+function CamPosi1(){
+	new Ajax.Request('control.py?q=posi1', {method: 'GET'});
+	debug_text.value += "Cam Posi 1" + "\n";
+	
+	document.getElementById('actiontext').innerHTML = 'Current action:';
+	var textarea = document.getElementById('debug_text');
+	textarea.scrollTop = textarea.scrollHeight- textarea.clientHeight;
+}
+
+//
+// General controls
+//
+
+function GoForward(){
+
+	document.getElementById('actiontext').innerHTML = 'Current action: Going Forward';
+	document.getElementById('keyboard_button_w').style.backgroundColor='#4D4D4D';
+	new Ajax.Request('control.py?q=w', {method: 'GET'});
+	debug_text.value += "Current action: Going Forward" + "\n";
+}
+function TurnLeft(){
+
+	document.getElementById('actiontext').innerHTML = 'Current action: Turning Left';
+	document.getElementById('keyboard_button_a').style.backgroundColor='#4D4D4D';
+	new Ajax.Request('control.py?q=a', {method: 'GET'});
+	debug_text.value += "Current action: Turning Left" + "\n";
+}
+function Reverse(){
+
+	document.getElementById('actiontext').innerHTML = 'Current action: Reversing';
+	document.getElementById('keyboard_button_s').style.backgroundColor='#4D4D4D';
+	new Ajax.Request('control.py?q=s', {method: 'GET'});
+	debug_text.value += "Current action: Reversing" + "\n";
+}
+function TurnRight(){
+
+	document.getElementById('actiontext').innerHTML = 'Current action: Turning Right';
+	document.getElementById('keyboard_button_d').style.backgroundColor='#4D4D4D';
+	new Ajax.Request('control.py?q=d', {method: 'GET'});
+	debug_text.value += "Current action: Turning Right" + "\n";
+	
+}
+
+//
+//Using Keyboard
+//
+
+function UseQ(){
+
+	document.getElementById('actiontext').innerHTML = 'Current action: Enable Q';
+	document.getElementById('keyboard_button_placeholder_q').style.backgroundColor='#4D4D4D';
+	new Ajax.Request('control.py?q=q', {method: 'GET'});
+	debug_text.value += "Current action: Enable Q" + "\n";
+	
+}
+function UseE(){
+
+	document.getElementById('actiontext').innerHTML = 'Current action: Enable E';
+	document.getElementById('keyboard_button_placeholder_e').style.backgroundColor='#4D4D4D';
+	document.getElementById('video_stream').style.display='block';
+	new Ajax.Request('control.py?q=e', {method: 'GET'});
+	debug_text.value += "Current action: Enable E" + "\n";
+	
+}
+function EnableQE(){
+
+	document.getElementById('keyboard_button_placeholder_q').style.backgroundColor='#858585';
+	document.getElementById('keyboard_button_placeholder_e').style.backgroundColor='#858585';
+	
+	document.getElementById('extra_button_text_q').innerHTML = 'Q';
+	document.getElementById('extra_button_text_e').innerHTML = 'E';
+	
+	document.getElementById('keyboard_button_placeholder_q').style.border='1px solid';
+	document.getElementById('keyboard_button_placeholder_e').style.border='1px solid';
+	
+	document.getElementById('extra_button_text_q').style.color='#000000';
+	document.getElementById('extra_button_text_e').style.color='#000000';
+	document.getElementById('keyboard_button_placeholder_q').style.margin='5px';
+	document.getElementById('keyboard_button_placeholder_e').style.margin='5px';
+}
+function ResetQE(){
+
+	document.getElementById('keyboard_button_placeholder_q').style.backgroundColor='#fff';
+	document.getElementById('keyboard_button_placeholder_e').style.backgroundColor='#fff';
+	
+	document.getElementById('keyboard_button_placeholder_q').style.margin='6px';
+	document.getElementById('keyboard_button_placeholder_e').style.margin='6px';
+	
+	document.getElementById('keyboard_button_placeholder_q').style.border='none';
+	document.getElementById('keyboard_button_placeholder_e').style.border='none';
+	
+	document.getElementById('extra_button_text_q').style.color='#fff';
+	document.getElementById('extra_button_text_e').style.color='#fff';
+}
+function RevertText(){
+
+	document.getElementById('keyboard_button_w').style.backgroundColor='#858585';
+	document.getElementById('keyboard_button_a').style.backgroundColor='#858585';
+	document.getElementById('keyboard_button_s').style.backgroundColor='#858585';
+	document.getElementById('keyboard_button_d').style.backgroundColor='#858585';
+	
+	new Ajax.Request('control.py?q=release', {method: 'GET'});
+
+}
+
+function KeyCheckKeyDown(){
+
+	var KeyID = event.keyCode;
+	
+	if(KeyID==87 && checkifactive != true) //W
+	{
+		checkifactive = true;
+		keyNUM = event.keyCode;
+		GoForward();
+	}
+
+	if(KeyID==65 && checkifactive != true)//A
+	{ 
+		checkifactive = true;
+		keyNUM = event.keyCode;
+		TurnLeft();
+	}
+	
+	else if(KeyID==83 && checkifactive != true)//S
+	{ 
+		checkifactive = true;
+		keyNUM = event.keyCode;
+		Reverse();
+	}
+	
+	else if(KeyID==68 && checkifactive != true)//D
+	{ 
+		checkifactive = true;
+		keyNUM = event.keyCode;
+		TurnRight();
+	}
+	
+	else if(KeyID==81 && checkextra == true)
+	{
+		if(enabledQ == false)
+		{
+			UseQ();
+			enabledQ = true;
+		}
+
+		else if(enabledQ = true)
+		{
+			document.getElementById('keyboard_button_placeholder_q').style.backgroundColor='#858585';
+			new Ajax.Request('control.py?q=q_release', {method: 'GET'});
+			enabledQ = false;
+			debug_text.value += "Current action: Disable Q" + "\n";
+			document.getElementById('actiontext').innerHTML = 'Current action: Disable Q';
+		}
+	}
+
+	else if(KeyID==69 && checkextra == true)
+	{
+		if(enabledE == false)
+		{
+			UseE();
+			enabledE = true;
+		}
+		else if(enabledE = true)
+		{
+			document.getElementById('keyboard_button_placeholder_e').style.backgroundColor='#858585';
+			document.getElementById('video_stream').style.display='none';
+			new Ajax.Request('control.py?q=e_release', {method: 'GET'});
+			enabledE = false;
+			document.getElementById('actiontext').innerHTML = 'Current action: Disable E';
+			debug_text.value += "Current action: Disable E" + "\n";
+		}
+	}
+
+	else if(KeyID==16) //Shift
+	{
+		if(checkextra == false)
+		{
+			EnableQE();
+			checkextra = true;
+		}
+		else if(checkextra == true)
+		{
+			ResetQE();
+			document.getElementById('extra_button_text_e').innerHTML = '';
+			document.getElementById('extra_button_text_q').innerHTML = '';
+			checkextra = false;
+		}
+	}
+	
+	else if(KeyID==220) //§§§§
+	{	
+		if(checkdebug == false)
+		{
+			document.getElementById('debug').style.display='block';
+			checkdebug = true;
+		}
+		else if(checkdebug == true)
+		{
+			document.getElementById('debug').style.display='none';
+			checkdebug = false;
+		}		
+	}
+	
+	var textarea = document.getElementById('debug_text');
+	textarea.scrollTop = textarea.scrollHeight- textarea.clientHeight;
+}
+
+function KeyCheckKeyUp(){
+	var KeyID = event.keyCode;
+	
+	if(KeyID==keyNUM)
+	{
+		RevertText();
+		checkifactive = false;
+		keyNUM == 00;
+	}
+	
+	document.getElementById('actiontext').innerHTML = 'Current action:';
+	var textarea = document.getElementById('debug_text');
+	textarea.scrollTop = textarea.scrollHeight- textarea.clientHeight;
+}
